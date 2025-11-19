989e72b61efb7879c21f662a31f7a9ed53115f08
Raman Chauhan <ramanchauhan13@gmail.com>
2025-11-19 12:30:03 +0530

    feat: add Swagger documentation and integrate Swagger UI
    - Updated package.json to include express-jsdoc-swagger and swagger-ui-express.
    - Enhanced server.js to serve Swagger documentation from ./docs/swagger.json.
    - Created swagger.json for API documentation covering authentication and user routes.

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import cors from 'cors';
 import 'dotenv/config';
 import './utils/cron.js';
 
+import fs from "fs";
+import swaggerUI from "swagger-ui-express";
+
+// Read swagger json
+const swaggerDocument = JSON.parse(
+  fs.readFileSync("./docs/swagger.json", "utf8")
+);
+
 // Route Imports
 import authRoutes from './routes/authRoutes.js';
 import salonAdminRoutes from './routes/salonAdminRoutes.js';
@@ -16,30 +24,29 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Swagger UI (your external docs)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+  });
 }).catch((error) => {
-    console.error('Error connecting to the database:', error);
-    process.exit(1);
+  console.error('Error connecting to the database:', error);
+  process.exit(1);
 });
 
 // Default Route
 app.get('/', (req, res) => {
-    res.send('Hello From Server');
+  res.send('Hello From Server');
 });
 
-// Route Usage
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/super-admin', superAdminRoutes);
 app.use('/api/salon-admin', salonAdminRoutes);
 app.use('/api/user', userRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).json({ message: 'Route not found' });
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
-
-
-
